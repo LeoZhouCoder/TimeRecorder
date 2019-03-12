9f640fda19404995ac3c842cf52a8a7680171cdc
LeoZhou1986 <leo.zhou1986@hotmail.com>
2019-03-13 10:43:13 +1300

    Add TimeRecord project

---
+++ TimeRecorder/TimeRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeRecorder
+//
+//  Created by Leo Zhou on 2019/3/13.
+//  Copyright © 2019 LeoZhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
