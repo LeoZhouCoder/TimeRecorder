126dc6ab794ee46f9a30afc728152d2ad0c497e3
LeoZhou1986 <leo.zhou1986@hotmail.com>
2019-03-14 15:52:46 +1300

    Add options to setting page.

--- TimeRecorder/TimeRecorder/Modules/Setting/SettingViewController.swift
+++ TimeRecorder/TimeRecorder/Modules/Setting/SettingViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
-    var info = [["Groups"],["Items0","Items1","Items2","Items3","Items4","Items5","Items6","Items7","Items8","Items0","Items1","Items2","Items3","Items4","Items5","Items6","Items7","Items8"]]
+    // Setting page items, currently only one option: edit Categories
+    var info = [["Categories"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,25 +20,19 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
         self.navigationController!.navigationBar.isTranslucent = false
         
         let screenSize = UIScreen.main.bounds.size
-        print("SettingViewController")
-        print(screenSize)
+        let tableViewHeight = screenSize.height - self.tabBarController!.tabBar.frame.height
         let tableView = UITableView(
-            frame: CGRect(x: 0, y: 0, width: screenSize.width,height: screenSize.height - self.tabBarController!.tabBar.bounds.height),
+            frame: CGRect(x: 0, y: 0, width: screenSize.width,height: tableViewHeight),
             style: .grouped)
-        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         tableView.delegate = self
         tableView.dataSource = self
-        
         tableView.separatorStyle = .singleLine
-        //tableView.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         tableView.allowsSelection = true
         tableView.allowsMultipleSelection = false
-        
         self.view.addSubview(tableView)
     }
     
-    // return the cell number for each section
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return info[section].count
     }
@@ -52,12 +47,12 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
         return cell
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    /*func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let name = info[indexPath.section][indexPath.row]
             print("delete \(name)")
         }
-    }
+    }*/
     
     // action when selected cell
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -69,8 +64,6 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, accessoryButtonTapped indexPath: IndexPath) {
         let name = info[indexPath.section][indexPath.row]
         print("selected \(name)")
-        
-        //self.pushViewController(GroupEditViewController(), animated: true)
         self.present(GroupEditViewController(), animated: true, completion: nil)
     }
     
@@ -97,15 +90,4 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath)-> CGFloat {
         return 50
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
